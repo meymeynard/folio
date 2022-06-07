db18fbd572befb526ea6b6a20c53e0db10f89d70
meymeynard <meymeynard@gmail.com>
2022-06-08 00:21:20 +0800

    chore: add text

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,10 +4,21 @@ import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponent
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Hello there!
+      </SectionTitle>
+      <SectionText>
+        I'm a web developer based in Manila with 9 years of relevant experience.
+        <br />I've worked with different startups as: frontend developer, backend developer, android developer and a little bit of UX research.
+      </SectionText>
+      <Button onClick={() => window.location = '#'}>
+        Learn More
+      </Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
